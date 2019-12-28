b9c6796e6a04e56a787cd64fc12427d06d3e1c5d
teja0594 <kedariteja1999@gmail.com>
2019-12-28 14:00:28 +0530

    Teja

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class add
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number 1:");
+		int a=sc.nextInt();
+		System.out.print("Enter Number 2:");
+		int b=sc.nextInt();
+		int c = a+b;
+		System.out.print("Addition of "+a+" and "+b+" is "+c);
+
+	}
+}
+
